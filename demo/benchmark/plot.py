b20d360c2863854b5b53d9bef6b501f6b11fb60d
v4hn <me@v4hn.de>
2024-11-15 13:57:15 +0100

    update benchmark

--- demo/benchmark/plot.py
+++ demo/benchmark/plot.py
@@ -8,34 +8,36 @@ import io
 
 # get OUT from argv[1], else use default 'out'
 import sys
-OUT = 'out'
+
+OUT = "out"
 if len(sys.argv) > 1:
     OUT = sys.argv[1]
 
 # SED_CMD=R"s/.*_\(.*\)workers.*took \(.*\)ms to find \(.*\) solution.*/\1,\2,\3/"
-SED_CMD=R"s/.*_\(.*\)workers.*took \(.*\)ms to find \(.*\) solution.*best solution \([0-9\.]*\).*/\1,\2,\3,\4/"
-CMD=f"""
+SED_CMD = R"s/.*_\(.*\)workers.*_\(.*\)solutions_.*took \(.*\)ms to find \(.*\) solution.*best solution \([0-9\.]*\).*/\1,\2,\3,\4,\5/"
+CMD = f"""
 grep -r "Planning took" {OUT}/ | sed '{SED_CMD}'
 """
 
 proc = subprocess.Popen(CMD, shell=True, stdout=subprocess.PIPE)
 output = proc.stdout.read()
-df = pd.read_csv(io.StringIO(output.decode('utf-8')), header=None)
-df.columns = ['workers', 'time', 'solutions', 'best cost']
+df = pd.read_csv(io.StringIO(output.decode("utf-8")), header=None)
+df.columns = ["workers", "required solutions", "time", "solutions", "best cost"]
 
-print(output.decode('utf-8'))
+print(output.decode("utf-8"))
 print(df)
 
 # add 'time / solutions' column to the dataframe
 
-df['time/solutions'] = df['time'] / df['solutions']
-df['time(s)'] = df['time'] / 1000
+df["time/solutions"] = df["time"] / df["solutions"]
+df["time(s)"] = df["time"] / 1000
 
-df.to_json(f'{OUT}/df.json', orient='records')
+df.to_json(f"{OUT}/df.json", orient="records")
 
 # make 0 inf workers infinite
 import math
-df['workers'] = df['workers'].replace(0, math.inf)
+
+df["workers"] = df["workers"].replace(0, math.inf)
 
 # plot stripplots for all three columns into a figure
 
@@ -51,18 +53,18 @@ fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 # axs[1].set_title('time(s)')
 # mean/variance plot instead:
 
-sns.violinplot(x='workers', y='time(s)', data=df, ax=axs[0])
-axs[1].set_title('time(s)')
+sns.violinplot(x="workers", y="time(s)", data=df, ax=axs[0])
+axs[1].set_title("time(s)")
 
-sns.violinplot(y='solutions', x= 'workers', data=df, ax=axs[1])
-axs[1].set_title('solutions')
+sns.violinplot(y="solutions", x="workers", data=df, ax=axs[1])
+axs[1].set_title("solutions")
 
 # sns.scatterplot(y='time/solutions', x='workers', data=df, ax=axs[3])
 # axs[3].set_title('time/solutions')
 # mean/variance plot instead:
 
-sns.violinplot(x='workers', y='time/solutions', data=df, ax=axs[2])
-axs[2].set_title('time/solutions')
+sns.violinplot(x="workers", y="time/solutions", data=df, ax=axs[2])
+axs[2].set_title("time/solutions")
 
 # rotate x labels
 
@@ -75,9 +77,9 @@ for ax in axs:
 for ax in axs[1:]:
     # set integer labels
     labels = [item.get_text() for item in ax.get_xticklabels()]
-    if labels[0] == '-1.0':
-        labels[0] = 'seq'
+    if labels[0] == "-1.0":
+        labels[0] = "seq"
     ax.set_xticklabels(labels)
 
-plt.savefig(f'{OUT}/plot.svg')
+plt.savefig(f"{OUT}/plot.svg")
 plt.show()
--- demo/benchmark/run_matrix.sh
+++ demo/benchmark/run_matrix.sh
@@ -5,6 +5,7 @@ OUT=${1:-out}
 mkdir -p $OUT
 
 parallel --bar --eta -j1 --shuf --header : \
-   "roslaunch moveit_task_constructor_demo pickplace.launch execute:=false keep_running:=false connect_compute_attempts:={workers} workers:={workers} > $OUT/pick_and_place_{workers}workers_rep{repetition}.log 2>&1" \
+   "roslaunch moveit_task_constructor_demo pickplace.launch execute:=false keep_running:=false max_solutions:={solutions} connect_compute_attempts:={workers} workers:={workers} > $OUT/pick_and_place_{workers}workers_{solutions}solutions_rep{repetition}.log 2>&1" \
    ::: workers $(seq -1 7) \
-   ::: repetition $(seq 10)
+   ::: repetition $(seq 10) \
+   ::: solutions 1 10 100
