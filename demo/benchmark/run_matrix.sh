3fd960116803def4b12a621a8762f8c255e5bcd7
v4hn <me@v4hn.de>
2024-12-02 18:18:29 +0100

    hack: parallelize benchmark

--- demo/benchmark/run_matrix.sh
+++ demo/benchmark/run_matrix.sh
@@ -4,8 +4,8 @@ OUT=${1:-out}
 
 mkdir -p $OUT
 
-parallel --bar --eta -j1 --shuf --header : \
-   "roslaunch moveit_task_constructor_demo pickplace.launch execute:=false keep_running:=false max_solutions:={solutions} connect_compute_attempts:={workers} workers:={workers} > $OUT/pick_and_place_{workers}workers_{solutions}solutions_rep{repetition}.log 2>&1" \
-   ::: workers $(seq -1 7) \
+parallel --bar --eta -j3 --shuf --header : \
+   "roslaunch moveit_task_constructor_demo pickplace.launch execute:=false keep_running:=false name:=mtc_tutorial{#} max_solutions:={solutions} connect_compute_attempts:={workers} workers:={workers} > $OUT/pick_and_place_{workers}workers_{solutions}solutions_rep{repetition}.log 2>&1" \
+   ::: workers -1 $(seq 1 15) \
    ::: repetition $(seq 10) \
-   ::: solutions 1 10 100
+   ::: solutions 1 10 100 1000
