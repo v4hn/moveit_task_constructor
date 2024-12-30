9727d5564407a560d77fcb50313f71a938adcfb0
v4hn <me@v4hn.de>
2024-12-30 02:27:49 +0100

    adjust launch parameter
    and default to 1 again, as naively setting the upper bound to the
    workers drops observed speedup.

--- demo/benchmark/run_matrix.sh
+++ demo/benchmark/run_matrix.sh
@@ -5,7 +5,7 @@ OUT=${1:-out}
 mkdir -p $OUT
 
 parallel --bar --eta -j3 --shuf --header : \
-   "roslaunch moveit_task_constructor_demo pickplace.launch execute:=false keep_running:=false name:=mtc_tutorial{#} max_solutions:={solutions} connect_compute_attempts:={workers} workers:={workers} > $OUT/pick_and_place_{workers}workers_{solutions}solutions_rep{repetition}.log 2>&1" \
-   ::: workers -1 $(seq 1 15) \
-   ::: repetition $(seq 10) \
-   ::: solutions 1 10 100 1000
+   "roslaunch moveit_task_constructor_demo pickplace.launch execute:=false keep_running:=false name:=mtc_tutorial{#} max_solutions:={solutions} workers:={workers} > $OUT/pick_and_place_{workers}workers_{solutions}solutions_rep{repetition}.log 2>&1" \
+   ::: workers -1 $(seq 2 10) \
+   ::: repetition $(seq 20) \
+   ::: solutions 1 10 100
